dcc2c58129d245f3e56267d05b29407437eba7ee
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2016-07-04 17:46:51 -0600

    Add CSS and assets loaders

--- config/webpack.js
+++ config/webpack.js
@@ -23,7 +23,39 @@ module.exports.webpack = {
         jQuery: 'jquery',
         'window.jQuery': 'jquery'
       })
-    ]
+    ],
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css?root=' + __dirname + '../assets', 'autoprefixer?browsers=last 2 versions'],
+        },
+        {
+          test: /\.png$/,
+          loader: "url?limit=100000"
+        },
+        {
+          test: /\.(jpg|gif)$/,
+          loader: "file"
+        },
+        {
+          test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url?limit=10000&mimetype=application/font-woff'
+        },
+        {
+          test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url?limit=10000&mimetype=application/octet-stream'
+        },
+        {
+          test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'file'
+        },
+        {
+          test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url?limit=10000&mimetype=image/svg+xml'
+        }
+      ]
+    }
   },
   watchOptions: {
     aggregateTimeout: 300
